8aa92b9094c9e6c87329fcbb4119761cdfbb249d
Josue-Figueroa <jfigueroar1900@alumno.ipn.mx>
2020-11-19 13:33:38 -0600

    Pa' que no me pierda,xd

---
+++ Segundapartemetodos/General.java
@@ -0,0 +1,10 @@
+/*
+Este sera una clase general, donde heredaremos algunos atributos
+que nos ayudaran a los programas 
+*/
+public class General{
+
+    public General(){
+        //Constructor
+    }
+}
--- Segundapartemetodos/Menu.java
+++ Segundapartemetodos/Menu.java
@@ -2,25 +2,52 @@
 import javax.swing.JOptionPane;
 //Clase del menu
 public class Menu{
-     //Atributos
+
+    public Menu(){
+        //Constructor
+    } 
+    
+    //Atributos
      private int opc;
      private char repe;
+     
  
      //Indice de los programas
      public void indice(){
-         JOptionPane.showMessageDialog(null, "3IV8_FIGUEROA_RIOS_JOSUE.");
-         JOptionPane.showMessageDialog(null, "Elija la opcion deseada: ");
-         String opcion = JOptionPane.showInputDialog(null, "1.- Edad de la persona. \n2.- Area y perimetro de un circulo o un rectangulo o un cuadrado o un triangulo. \n3.- Llamadas telefonicas.", "4.- Salir.");
-         int opc = Integer.parseInt(opcion);
- 
+         //Variables
+        String opcion;
+        int opc;
+        JOptionPane.showMessageDialog(null, "3IV8_FIGUEROA_RIOS_JOSUE.");
+        JOptionPane.showMessageDialog(null, "Elija la opcion deseada: "); 
+
+        //Generamos un bloque try catch, para el manejo de alguna excepcion
+        try {
+        opcion = JOptionPane.showInputDialog(null, "1.- Sueldo de empleado. \n2.- Proovedores. \n3.- Libros de una biblioteca.", "4.- Salir.");
+        opc = Integer.parseInt(opcion);
+        } catch (Exception e) {
+        System.out.pritnln("Error: "+e);
+        opcion = JOptionPane.showInputDialog(null, "Solo ingresa el numero de la opcion deseada: \n1.- Sueldo de empleado. \n2.- Proovedores. \n3.- Libros de una biblioteca.", "\n4.- Salir. ");
+        opc = Integer.parseInt(opcion);
+        }
+
          //Creamos dos bucles por si ingresa valores incorrectos
          while(opc>4){
-             System.out.println("Ha ingresado un numero incorrecto. Elija la opcion deseada: ");
-             
+            opcion = JOptionPane.showInputDialog("Ha ingresado un numero incorrecto. Elija la opcion deseada: \n1.- Sueldo de empleado. \n2.- Proovedores. \n3.- Libros de una biblioteca.", "\n4.- Salir.");
+            opc = Integer.parseInt(opcion);
          }
          while(opc<0){
-             System.out.println("Ha ingresado un numero incorrecto. Elija la opcion deseada: ");
-             
+            opcion = JOptionPane.showInputDialog(null, "Ha ingresado un numero incorrecto. Elija la opcion deseada: \n1.- Sueldo de empleado. \n2.- Proovedores. \n3.- Libros de una biblioteca.", "\n4.- Salir. ");
+            opc = Integer.parseInt(opcion);
          }
+
+         //Cramos el switch del menu
+        switch (opc) {
+            case 1:
+                
+            break;
+        
+            default:
+            JOptionPane.showMessageDialog(null, "Hasta luego 'Spartan'.");
+        }
     }
 }
