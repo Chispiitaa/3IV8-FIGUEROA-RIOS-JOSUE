27c21bcee2fca4dae8e59d1296f741fcc8d06163
Josue-Figueroa <jfigueroar1900@alumno.ipn.mx>
2020-11-20 13:27:14 -0600

    Avances

--- Segundapartemetodos/General.java
+++ Segundapartemetodos/General.java
@@ -4,7 +4,24 @@ que nos ayudaran a los programas
 */
 public class General{
 
+    //Atributos
+    private String nombre;
+    
     public General(){
         //Constructor
+    }
+
+    //Acceder a las variables
+    public General(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //el metodo get
+    public String getNombre(){
+        return nombre;
+    }
+    //El metodo set
+    public void setName(String nombre) {
+        this.nombre = nombre;
     }
 }
